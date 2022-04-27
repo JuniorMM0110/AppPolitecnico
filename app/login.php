19b665e5298c535cceb4af26077b47696d81cdaa
JuniorMM0110 <morajunior42@gmail.com>
2022-04-26 20:41:38 -0400

    Creación del login e inicio del index
    Cree la parte del login y tambien lo de registrar el usuario en caso de no tener cuenta. Ahora voy a empezar con el como se vera el index

---
+++ app/login.php
@@ -0,0 +1,27 @@
+<?php
+require("../setup/datosConexion.php");
+
+$conexion = new Conexion();
+$conex = $conexion->conexion();
+
+
+$usuario = $_POST['username'];
+$cont = $_POST['password'];
+
+$sql = "SELECT usuario,contraseña FROM ventas";
+$llamar = $conex->prepare($sql);
+
+#$username = $_GET['usuario'];
+#$contraseña = $_GET['contraseña'];
+
+$query = $conex->prepare("SELECT * FROM ulogin ");
+$query->execute();
+
+$result = $query->fetch(PDO::FETCH_ASSOC);
+
+if($usuario == $result['usuario'] || $cont == $result['contraseña']){
+	header("location:../index.php");
+}else{
+	echo "NO";
+}
+?>
